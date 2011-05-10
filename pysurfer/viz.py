bb870bcbcfa20d54bd12350a6acd2fca56997c92
Michael Waskom <mwaskom@mit.edu>
2011-05-10 12:43:20 -0400

    Added initial overlay object

--- pysurfer/viz.py
+++ pysurfer/viz.py
@@ -5,6 +5,7 @@ from os.path import join as pjoin
 import numpy as np
 from enthought.mayavi import mlab
 
+from . import io
 from .io import Surface
 
 
@@ -71,7 +72,7 @@ class Brain(object):
         # Turn disable render off so that it displays
         self._f.scene.disable_render = False
 
-    def add_overlay(self, filepath, range, sign="abs", name=None):
+    def add_overlay(self, filepath, range, sign="abs", name=None, visible=True):
         """Add an overlay to the overlay dict.
 
         Parameters
@@ -84,6 +85,8 @@ class Brain(object):
             whether positive, negative, or both values should be displayed
         name : str
             name for the overlay in the internal dictionary
+        visible : boolean
+            whether the overlay should be visible upon load
 
         """
         if name is None:
@@ -98,6 +101,10 @@ class Brain(object):
 
         if not sign in ["abs", "pos", "neg"]:
             raise ValueError("Overlay sign must be 'abs', 'pos', or 'neg'")
+
+        self._f.scene.disable_render = False
+        self.overlays[name] = Overlay(self._geo, filepath, range, sign) 
+        self._f.scene.disable_render = True
 
     def __get_geo_colors(self):
         """Return an mlab colormap name, vmin, and vmax for binary curvature.
@@ -121,10 +128,31 @@ class Brain(object):
 
 class Overlay(object):
 
-    def __init__(self, subject_id, hemi, surf, filepath, range, sign):
+    def __init__(self, geo, filepath, range, sign):
         """
         """
         if sign in ["abs", "pos"]:
-            self._pos = Surface(subject_id, hemi, surf)
+            pos_stats = io.load_scalar_data(filepath)
+            pos_mesh = mlab.pipeline.triangular_mesh_source(geo.x, geo.y, geo.z,
+                                                            geo.faces,
+                                                            scalars=pos_stats)
+            pos_thresh = mlab.pipeline.threshold(pos_mesh, low=range[0])
+            pos_surf = mlab.pipeline.surface(pos_thresh, colormap="YlOrRd", vmin=range[0], vmax=range[1])
+            pos_bar = mlab.colorbar(pos_surf)
+            pos_bar.reverse_lut=True
+            pos_bar.visible = False
+            
+            self.pos = pos_surf
+
         if sign in ["abs", "neg"]:
-            self._neg = Surface(subject_id, hemi, surf)
+            neg_stats = io.load_scalar_data(filepath)
+            neg_mesh = mlab.pipeline.triangular_mesh_source(geo.x, geo.y, geo.z,
+                                                            geo.faces,
+                                                            scalars=neg_stats)
+            neg_thresh = mlab.pipeline.threshold(neg_mesh, low=range[0])
+            neg_surf = mlab.pipeline.surface(neg_thresh, colormap="Blues", vmin=range[0], vmax=range[1])
+            neg_bar = mlab.colorbar(neg_surf)
+            neg_bar.reverse_lut=True
+            pos_bar.visibile = False
+
+            self.neg = neg_surf
