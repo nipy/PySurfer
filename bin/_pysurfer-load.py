cea76f029e7ae8354806905a88725dbc45977526
Michael Waskom <mwaskom@mit.edu>
2011-05-17 19:54:42 -0400

    Adding functional command-line interface scripts

--- bin/_pysurfer-load.py
+++ bin/_pysurfer-load.py
@@ -1,18 +1,37 @@
 #! /usr/bin/env python
-import sys
-import argparse
-from surfer import Brain
+"""
+This is the intermediate (hidden) command-line interface script
+for PySurfer. It is executed as a run-time script with IPython
+by the pysurfer executable, and it is in this script that the
+visualiztion environment is actually set up.
 
-parser = argparse.ArgumentParser()
-parser.add_argument("subject_id")
-parser.add_argument("hemi", choices=["lh", "rh"])
-parser.add_argument("surf")
-parser.add_argument("-overlay")
-parser.add_argument("-fminmax", nargs=2, default=(2, 5))
-parser.add_argument("-sign", default="abs")
+"""
+import sys
+from surfer import Brain
+from surfer._commandline import parser
 
 args = parser.parse_args(sys.argv[1].split())
 
-brain = Brain(args.subject_id, args.hemi, args.surf)
+# Load  up the figure and underlying brain object
+b = Brain(args.subject_id, args.hemi, args.surf, args.curv, args.title)
+
+# Maybe load some morphometry
+if args.morphometry is not None:
+    b.add_morphometry(args.morphometry)
+
+# Maybe load an overlay
 if args.overlay is not None:
-    brain.add_overlay(args.overlay, args.fminmax, args.sign)
+    if args.range is not None:
+        args.min, args.max = args.range
+
+    b.add_overlay(args.overlay, args.min, args.max, args.sign)
+
+# Also point brain at the Brain() object
+brain = b
+
+# It's nice to have mlab in the namespace, but we'll import it
+# after the other stuff so getting usage is not interminable
+from enthought.mayavi import mlab
+
+# Now clean up the namespace a bit
+del parser, args
