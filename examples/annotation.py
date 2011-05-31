055966bb3cf16d3d0ef5e03c5da85f6479ae6a0e
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-05-31 14:46:43 -0400

    FIX : kwarg pb with annot contour in example

--- examples/annotation.py
+++ examples/annotation.py
@@ -14,7 +14,7 @@ data_path = pjoin(subj_dir, subject_id)
 annot_path = pjoin(data_path, "label", "%s.aparc.annot" % "lh")
 
 brain = viz.Brain(sub, hemi, surf)
-brain.add_annotation(annot_path, contour=True)
+brain.add_annotation(annot_path, borders=True)
 # show all views
 brain.show_view('lateral')
 
