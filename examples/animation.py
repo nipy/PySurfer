e40275250fc2455f0d1caed712bf4d0ef5e8678e
Scott Burns <scott.s.burns@gmail.com>
2011-06-22 13:13:27 -0400

    animation still not working

--- examples/animation.py
+++ examples/animation.py
@@ -33,52 +33,80 @@ from surfer import viz
 from enthought.tvtk.api import tvtk
 import numpy as np
 from enthought.mayavi import mlab
+import math
 
-brain = viz.Brain('bert', 'lh', 'inflated')
-
-def norm(q):
-	mag = np.sqrt(sum(np.array(q) ** 2))
-	return tuple(np.array(q) / mag)
-
-def v2q(brain, v):
-	q = {'dorsal': (0., 0., 1, 0.),
-		 'ventral': (0., 0., -1., 0.),
-		 'lateral': (0., 1., 0., 0.),
-		 'medial': (0., -1., 0., 0.),
-		 'anterior': (0., 0., 0., 1.),
-		 'ventral': (0., 0., 0., -1.)}
-	#gv = brain.xfm_view(v)
-	return norm(q[v])	
-	
-def q2v(brain, q):
-	pass
-
-def show_q(brain, q):
-	pass
-
-qint = tvtk.QuaternionInterpolator()
+brain = viz.Brain('fsaverage', 'lh', 'inflated')
 
 
-f = 'lateral'
-l = 'medial'
+first = 'lateral'
+last = 'medial'
 
 n = 60
 
-brain.show_view(f)
-q1 = brain._f.scene.camera.orientation_wxyz
-brain.show_view(l)
-q2 = brain._f.scene.camera.orientation_wxyz
+def from_axis_angle(wxyz):
+    w, x, y, z = wxyz
+    theta = np.radians(w)
+    c = np.cos(theta/2)
+    s = np.sin(theta/2)
+    r = np.hypot(np.hypot(x, y), z)
+    q = np.array((c, s*x/r, s*y/r, s*z/r))
+    return q
 
-qint.add_quaternion(0, q1)
-qint.add_quaternion(1, q2)
+def as_axis_angle(q):
+    c,x,y,z = q
+    angle = 2.0 * math.acos(c)
+    if c > (1.0 - 1e-14) or c < (-1.0 + 1e-14):
+       axis = (1.0, 0.0, 0.0)
+    else:
+       r = np.hypot(np.hypot(x,y),z)
+       axis = (x/r, y/r, z/r)
+    return (np.degrees(angle),) + axis
 
-brain.show_view(f)
-for i in np.array(range(1, n)) / float(n):
+def qmul(q1, q2):
+    # Return q1 * q2
+    t1, x1, y1, z1 = q1
+    t2, x2, y2, z2 = q2
+    t = t1*t2 - x1*x2 - y1*y2 - z1*z2
+    x = t1*x2 + t2*x1 + y1*z2 - z1*y2
+    y = t1*y2 - x1*z2 + t2*y1 + z1*x2 
+    z = t1*z2 + x1*y2 - y1*x2 + t2*z1 
+    return (t, x, y, z)
+
+def qdiv(qnum, qden):
+    # Return qnum * qden^-1
+    m2 = (qden * qden).sum()
+    qinv = np.array([qden[0], -qden[1], -qden[2], -qden[3]]) / m2
+    return qmul(qnum, qinv)
+
+camera = brain._f.scene.camera
+
+brain.show_view(first)
+wxyz1 = camera.orientation_wxyz
+q1 = from_axis_angle(wxyz1)
+brain.show_view(last)
+wxyz2 = camera.orientation_wxyz
+q2 = from_axis_angle(wxyz2)
+
+dq = qdiv(q2, q1)
+qint = tvtk.QuaternionInterpolator()
+qint.add_quaternion(0, (1.0, 0.0, 0.0, 0.0))  # Identity
+qint.add_quaternion(1, dq)
+
+r = np.linspace(0, 1, n)[1:]
+brain.show_view(first)
+# Push the current transformation onto the stack so we can keep it for later.
+camera.view_transform_object.push()
+# And again, because we pop before we push.
+camera.view_transform_object.push()
+
+for i in r:
 	out = np.zeros(4)
 	qint.interpolate_quaternion(i, out)
-	print out
-# 	tr = tvtk.Transform()
-# 	tr.rotate_wxyz(*out)
-# 	brain._f.scene.camera.apply_transform(tr)
-	brain._f.scene.camera_
-# 	brain._f.scene.render()
+	tr = tvtk.Transform()
+	tr.rotate_wxyz(*as_axis_angle(out))
+	# Revert back to the original transform.
+	camera.view_transform_object.pop()
+	# Apply the new transform. this pushes on the stack
+	camera.apply_transform(tr)
+	brain._f.scene.renderer.reset_camera_clipping_range()
+	brain._f.scene.render()
