57e26d284d98f65c81c1b2f018c9f598939840b7
Michael Waskom <mwaskom@mit.edu>
2011-07-05 14:57:21 -0400

    Added Lancaster transform convenience function

--- surfer/utils.py
+++ surfer/utils.py
@@ -25,6 +25,30 @@ def find_closest_vertices(surface_coords, point_coords):
     return np.argmin(cdist(surface_coords, point_coords), axis=0)
 
 
+def tal_to_mni(coords):
+    """Convert Talairach coords to MNI using the Lancaster transform.
+
+    Parameters
+    ----------
+    coords : n x 3 numpy array
+        Array of Talairach coordinates
+
+    Returns
+    -------
+    mni_coords : n x 3 numpy array
+        Array of coordinates converted to MNI space
+
+    """
+    coords = np.atleast_2d(coords)
+    xfm = np.array([
+        [  1.06860,  -0.00396,   0.00826,   1.07816],
+        [  0.00640,   1.05741,   0.08566,   1.16824],
+        [ -0.01281,  -0.08863,   1.10792,  -4.17805],
+        [  0.00000,   0.00000,   0.00000,   1.00000] ])
+    mni_coords = np.dot(np.c_[coords, np.ones(coords.shape[0])], xfm.T)[:, :3]
+    return mni_coords
+
+
 def mesh_edges(faces):
     """Returns sparse matrix with edges as an adjacency matrix
 
