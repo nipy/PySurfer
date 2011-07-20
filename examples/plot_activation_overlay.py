44d5b4f5016bee127cfe1ccfdeb859a8bb7bde3d
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2011-07-20 09:51:52 -0400

    DOC: fix activation load in example + doc generation

--- examples/plot_activation_overlay.py
+++ examples/plot_activation_overlay.py
@@ -10,14 +10,14 @@ print __doc__
 
 import os.path as op
 from surfer import Brain
-# 
+
 brain = Brain("fsaverage", "lh", "inflated")
 
-overlay_file = op.join(op.dirname(__file__), "data/lh.sig.nii.gz")
+overlay_file = op.join("data", "lh.sig.nii.gz")
 
 brain.add_overlay(overlay_file, min=2, max=10, sign="abs")
 
-brain.overlay["sig"].remove()
+brain.overlays["sig"].remove()
 
 brain.add_overlay(overlay_file, min=4, max=15, sign="pos")
 
