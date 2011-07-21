5b0d28d3f831bc489b0783354d0efeec10739c86
Michael Waskom <mwaskom@mit.edu>
2011-07-21 19:23:23 -0400

    Example montage image is no longer huge

--- examples/plot_montage.py
+++ examples/plot_montage.py
@@ -18,14 +18,15 @@ brain = Brain(sub, hemi, surf)
 
 ###############################################################################
 # Save a set of images as a montage
-brain.save_montage('fsaverage_h_montage.png', ['l', 'v', 'm'], orientation='h')
+brain.save_montage('/tmp/fsaverage_h_montage.png', ['l', 'v', 'm'], orientation='v')
 brain.close()
 
 ###############################################################################
 # View created image
 import Image
 import pylab as pl
-image = Image.open('fsaverage_h_montage.png')
+image = Image.open('/tmp/fsaverage_h_montage.png')
+fig = pl.figure(figsize=(5,3))
 pl.imshow(image, origin='lower')
 pl.xticks(())
 pl.yticks(())
