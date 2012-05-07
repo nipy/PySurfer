ed4e27942ea5ac6f6e8bdf943644dc51a4d8cc30
Michael Waskom <mwaskom@stanford.edu>
2012-05-06 19:08:39 -0700

    FIX provide exeption info with surfer.ui cannot be imported

--- surfer/__init__.py
+++ surfer/__init__.py
@@ -2,8 +2,8 @@ from io import Surface
 from viz import Brain
 try:
     from gui import TimeViewer
-except SystemError:
+except SystemError as e:
     import warnings
-    warnings.warn("Could not import surfer/gui module")
+    warnings.warn("Could not import surfer.gui module: %s" % e)
 
 __version__ = "0.4.dev"
