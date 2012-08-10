e4f37e4bd5ec62ccea2fed243fef7ad89e4e48a1
Martin Luessi <mluessi@nmr.mgh.harvard.edu>
2012-08-10 15:47:27 -0400

    ENH: automatic # of smoothing steps

--- surfer/utils.py
+++ surfer/utils.py
@@ -81,13 +81,17 @@ def smoothing_matrix(vertices, adj_mat, smoothing_steps=20):
        for a subset of vertices onto mesh with an adjancency matrix given by
        adj_mat.
 
+       If smoothing_steps is None, as many smoothing steps are applied until
+       the whole mesh is filled with with non-zeros. Only use this option if
+       the vertices correspond to a subsampled version of the mesh.
+
     Parameters
     ----------
     vertices : 1d array
         vertex indices
     adj_mat : sparse matrix
         N x N adjacency matrix of the full mesh
-    smoothing_steps : int
+    smoothing_steps : int or None
         number of smoothing steps (Default: 20)
 
     Returns
@@ -105,18 +109,20 @@ def smoothing_matrix(vertices, adj_mat, smoothing_steps=20):
     e = e + sparse.eye(n_vertices, n_vertices)
     idx_use = vertices
     smooth_mat = 1.0
-    for k in range(smoothing_steps):
+    n_iter = smoothing_steps if smoothing_steps is not None else 1000
+    for k in range(n_iter):
         e_use = e[:, idx_use]
 
         data1 = e_use * np.ones(len(idx_use))
         idx_use = np.where(data1)[0]
-        scale_mat = sparse.dia_matrix((1 / data1[idx_use], 0), \
+        scale_mat = sparse.dia_matrix((1 / data1[idx_use], 0),
                                   shape=(len(idx_use), len(idx_use)))
 
         smooth_mat = scale_mat * e_use[idx_use, :] * smooth_mat
 
-        print "Smoothing matrix creation, step %d/%d" % \
-              (k + 1, smoothing_steps)
+        print "Smoothing matrix creation, step %d" % (k + 1)
+        if smoothing_steps is None and len(idx_use) >= n_vertices:
+            break
 
     # Make sure the smooting matrix has the right number of rows
     # and is in COO format
