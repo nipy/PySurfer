084a7cd4f5422a02b692fb5611737218eb18dbed
christianmbrodbeck <christianmbrodbeck@gmail.com>
2013-01-13 12:16:42 -0300

    ENH Brain.set_time(): don't depend on monotonously increasing time axis

--- surfer/viz.py
+++ surfer/viz.py
@@ -1358,10 +1358,10 @@ class Brain(object):
             raise RuntimeError("Brain has no time axis")
 
         # Check that time is in range
-        tmin = times[0]
-        tmax = times[-1]
-        if (tmin - time > (times[1] - tmin) / 2) or (time - tmax >
-                                                     (tmax - times[-2]) / 2):
+        tmin = min(times)
+        tmax = max(times)
+        max_diff = (tmax - tmin) / (len(times) - 1) / 2
+        if time < tmin - max_diff or time > tmax + max_diff:
             err = ("time = %s lies outside of the time axis "
                    "[%s, %s]" % (time, tmin, tmax))
             raise ValueError(err)
