d1008f77dbc54eda80bcec8d5ee69dbb1f054304
Morgan Hough <morgan.hough@gmail.com>
2013-02-07 15:23:39 -0800

    Update examples/plot_labels.py
    entorhinal label renamed entorhinal_exvivo

--- examples/plot_labels.py
+++ examples/plot_labels.py
@@ -44,4 +44,4 @@ brain.show_view("medial")
 # Use any valid matplotlib color.
 brain.add_label("V1", color="steelblue", alpha=.6)
 brain.add_label("V2", color="#FF6347", alpha=.6)
-brain.add_label("entorhinal", color=(.2, 1, .5), alpha=.6)
+brain.add_label("entorhinal_exvivo", color=(.2, 1, .5), alpha=.6)
