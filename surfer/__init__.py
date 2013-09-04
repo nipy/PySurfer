55bf681891c2517500695b3e5e7c4175f24ab8b7
Michael Waskom <mwaskom@stanford.edu>
2013-09-04 14:34:06 -0700

    Changing version string because of anal

--- surfer/__init__.py
+++ surfer/__init__.py
@@ -2,6 +2,6 @@ from io import Surface
 from viz import Brain, TimeViewer
 from utils import verbose, set_log_level, set_log_file
 
-__version__ = "0.5dev"
+__version__ = "0.5.dev"
 
 set_log_level()
