94009611eae72861fe663a7914903210f70b898f
Eric Larson <larson.eric.d@gmail.com>
2013-09-06 09:45:07 -0700

    ENH: Better Windows support

--- surfer/config.py
+++ surfer/config.py
@@ -2,7 +2,7 @@ import ConfigParser
 import os
 from StringIO import StringIO
 
-homedir = os.environ['HOME']
+homedir = os.getenv('APPDATA' if 'nt' == os.name.lower() else 'HOME', None)
 default_cfg = StringIO("""
 [visual]
 background = black
