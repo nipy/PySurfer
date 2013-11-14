e9ee10381268ed841057595339712c12ed00889e
Martin Luessi <mluessi@nmr.mgh.harvard.edu>
2013-11-14 16:36:15 -0500

    read version from __init__.py

--- surfer/__init__.py
+++ surfer/__init__.py
@@ -2,6 +2,6 @@ from io import Surface
 from viz import Brain, TimeViewer
 from utils import verbose, set_log_level, set_log_file
 
-from __version__ import __version__
+__version__ = "0.5.dev"
 
 set_log_level()
