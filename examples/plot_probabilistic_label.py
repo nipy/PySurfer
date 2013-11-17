079f1c0bfaa174c2ca9dfd56c1337661535e9662
mwaskom <mwaskom@stanford.edu>
2013-11-16 16:38:04 -0800

    Tweak probabilistic labels example

--- examples/plot_probabilistic_label.py
+++ examples/plot_probabilistic_label.py
@@ -1,4 +1,5 @@
 """
+============================
 Display Probabilistic Labels
 ============================
 
@@ -16,37 +17,39 @@ brain = Brain("fsaverage", "lh", "inflated",
               config_opts=dict(cortex="low_contrast"))
 
 """
-The simplest way is to use add_label.
+The easiest way to label any vertex that could be in the region is with
+add_label.
 """
-brain.add_label("BA1", color="darkblue")
+brain.add_label("BA1", color="#A6BDDB")
 
 """
-You can also threshold based on the probability of that
-region being at each vertex.
+You can also threshold based on the probability of that region being at each
+vertex.
 """
-brain.add_label("BA1", color="dodgerblue", scalar_thresh=.5)
+brain.add_label("BA1", color="#2B8CBE", scalar_thresh=.5)
 
 """
-It's also possible to plot just the label boundary, in case
-you wanted to overlay the label on an activation plot to
-asses whether it falls within that region.
+It's also possible to plot just the label boundary, in case you wanted to
+overlay the label on an activation plot to asses whether it falls within that
+region.
 """
-brain.add_label("BA45", color="firebrick", borders=True)
-brain.add_label("BA45", color="salmon", borders=True, scalar_thresh=.5)
+brain.add_label("BA45", color="#F0F8FF", borders=True, scalar_thresh=.5)
+brain.add_label("BA45", color="#F0F8FF", alpha=.3, scalar_thresh=.5)
 
 """
 Finally, with a few tricks, you can display the whole probabilistic map.
 """
-label_file = join(environ["SUBJECTS_DIR"],
-                  "fsaverage", "label", "lh.BA6.label")
+subjects_dir = environ["SUBJECTS_DIR"]
+label_file = join(subjects_dir, "fsaverage", "label", "lh.BA6.label")
+
 prob_field = np.zeros_like(brain._geo.x)
 ids, probs = io.read_label(label_file, read_scalars=True)
 prob_field[ids] = probs
-brain.add_data(prob_field, thresh=1e-5)
+brain.add_data(prob_field, thresh=1e-5, colormap="RdPu")
 
 """
-Adjust the colorbar to represent the coarseness of the probability
-estimates more closely.
+Adjust the colorbar to represent the coarseness of the probability estimates
+more closely.
 """
 brain.data["colorbar"].number_of_colors = 10
 brain.data["colorbar"].number_of_labels = 11
