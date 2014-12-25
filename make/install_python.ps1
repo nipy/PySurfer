e5e76d6fff7f70f6a867448e31a2031c45c0e6f3
Eric89GXL <larson.eric.d@gmail.com>
2014-12-25 07:56:58 -0800

    FIX?

--- make/install_python.ps1
+++ make/install_python.ps1
@@ -3,7 +3,41 @@
 # License: CC0 1.0 Universal: http://creativecommons.org/publicdomain/zero/1.0/
 
 $MINICONDA_URL = "http://repo.continuum.io/miniconda/"
-$BASE_URL = "https://www.python.org/ftp/python/"
+$FSAVERAGE_URL = "http://faculty.washington.edu/larsoner/fsaverage_min.tar.gz"
+
+function DownloadExtractFsaverage () {
+    $webclient = New-Object System.Net.WebClient
+    $basedir = $pwd.Path + "\"
+    $filepath = $basedir + "fsaverage_min.zip"
+    # Download and retry up to 3 times in case of network transient errors.
+    $url = $FSAVERAGE_URL
+    Write-Host "Downloading" $url
+    $retry_attempts = 2
+    for($i=0; $i -lt $retry_attempts; $i++){
+        try {
+            $webclient.DownloadFile($url, $filepath)
+            break
+        }
+        Catch [Exception]{
+            Start-Sleep 1
+        }
+    }
+    if (Test-Path $filepath) {
+        Write-Host "File saved at" $filepath
+    } else {
+        # Retry once to get the error message if any at the last try
+        $webclient.DownloadFile($url, $filepath)
+    }
+    # Now we extract
+    $subjects_dir = $basedir + "\subjects"
+    New-Item -ItemType directory -Path $subjects_dir
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace($filepath)
+    foreach($item in $zip.items())
+    {
+        $shell.Namespace($subjects_dir).copyhere($item)
+    }
+}
 
 
 function DownloadMiniconda ($python_version, $platform_suffix) {
@@ -33,14 +67,14 @@ function DownloadMiniconda ($python_version, $platform_suffix) {
         Catch [Exception]{
             Start-Sleep 1
         }
-   }
-   if (Test-Path $filepath) {
-       Write-Host "File saved at" $filepath
-   } else {
-       # Retry once to get the error message if any at the last try
-       $webclient.DownloadFile($url, $filepath)
-   }
-   return $filepath
+    }
+    if (Test-Path $filepath) {
+        Write-Host "File saved at" $filepath
+    } else {
+        # Retry once to get the error message if any at the last try
+        $webclient.DownloadFile($url, $filepath)
+    }
+    return $filepath
 }
 
 
@@ -86,6 +120,7 @@ function InstallMinicondaPip ($python_home) {
 
 
 function main () {
+    DownloadExtractFsaverage
     InstallMiniconda $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     InstallMinicondaPip $env:PYTHON
 }
