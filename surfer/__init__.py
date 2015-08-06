7faeabcdb8a0f6539768159364501ec2672cb19d
Michael Waskom <mwaskom@stanford.edu>
2015-08-06 16:31:26 -0700

    Bump version back to dev

--- surfer/__init__.py
+++ surfer/__init__.py
@@ -2,7 +2,7 @@ from .viz import Brain, TimeViewer  # noqa
 from .utils import Surface, verbose, set_log_level, set_log_file  # noqa
 from .io import project_volume_data  # noqa
 
-__version__ = "0.6"
+__version__ = "0.7.dev"
 
 set_log_file()  # initialize handlers
 set_log_level()  # initialize logging level
