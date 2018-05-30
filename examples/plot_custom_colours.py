9ea87ea841d01afb30e2fc949422e9f85759149f
LauraGwilliams <lgwilliams90@gmail.com>
2018-05-30 14:28:37 -0400

    FIX: add surf object to example_data and work from that rather than MNE

--- examples/plot_custom_colours.py
+++ examples/plot_custom_colours.py
@@ -17,16 +17,21 @@ from surfer import Brain
 print(__doc__)
 
 ###############################################################################
-# define functions
+# define helper functions
 
-def norm(x, lims=[1, 99]):
-    x = np.asarray(x, dtype=float)
-    m, M = np.percentile(x, lims)
-    x -= m
-    x /= M-m
-    x[x < 0] = 0
-    x[x > 1] = 1
-    return x
+def norm(x):
+	'''
+	Normalise array betweeen 0-1
+	'''
+	return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+###############################################################################
+# load surface
+
+surf_fname = '/Users/lauragwilliams/Documents/programming/PySurfer/examples/example_data/lh.white'
+rr, tris = nibabel.freesurfer.io.read_geometry(surf_fname)
+tris = tris.astype(np.uint32)
+x, y, z = rr.T
 
 ###############################################################################
 # generate an rgba matrix, of shape n_vertices x 4
@@ -34,66 +39,30 @@ def norm(x, lims=[1, 99]):
 # define color map
 cmap = plt.cm.rainbow
 
-# load data from mne sample dataset
-stc_dir = (mne.datasets.sample.data_path() +
-           '/MEG/sample/fsaverage_audvis-meg-lh.stc')
-stc = mne.read_source_estimate(stc_dir)
-data = stc.lh_data
-
-# extract latency idx of the peak in each source for *hue*
-hue = map(np.argmax, data)
-
-# extract actual value in the peak of each source for *alpha*
-alpha = norm([data[ii, w] for ii, w in enumerate(hue)])
-
-# normalise hue and alpha to be between 0-1
-alpha = norm(alpha, [1, 80])
-hue = norm(hue, [1, 80])
-
-# map the colormap to the vertex latency values
+# change colour based on position on the x axis
+hue = norm(x)
 colors = cmap(hue)[:, :3]
+
+# change alpha based on position on the z axis
+alpha = norm(z)
 
 # combine hue and alpha into a Nx4 matrix
 rgba_vals = np.concatenate((colors, alpha[:, None]), axis=1)
 
+
 ###############################################################################
-# plot the rgba values on a PySurfer brain
-
-verts = stc.vertices
-hemi='lh'
-surf='pial'
-
-# where to load up surfaces from
-subjects_dir = mne.datasets.sample.data_path() + '/subjects'
-
-# make colours copy
-colors = deepcopy(rgba_vals)
-n_verts = colors.shape[0]
-
-# load surface
-rr, tris = mne.read_surface('%s/fsaverage/surf/%s.%s' % (subjects_dir,
-                                                         hemi, surf))
-tris = tris.astype(np.uint32)
-x, y, z = rr.T
-
-# interpolate values from ico4 -> max
-adj_mat = mesh_edges(tris)
-smooth_mat = smoothing_matrix(verts[0], adj_mat,
-                              20, verbose=False)
-
-colors = smooth_mat.dot(colors[:n_verts])
+# plot
 
 # init figure
 fig = mlab.figure()
 b = Brain('fsaverage', hemi, surf, subjects_dir=subjects_dir,
-          background='white',
-          figure=fig)
+          background='white', alpha=0, figure=fig)
 
 # plot points in x,y,z
 mesh = mlab.pipeline.triangular_mesh_source(
     x, y, z, tris, figure=fig)
 mesh.data.point_data.scalars.number_of_components = 4  # r, g, b, a
-mesh.data.point_data.scalars = (colors * 255).astype('ubyte')
+mesh.data.point_data.scalars = (rgba_vals * 255).astype('ubyte')
 
 # tvtk for vis
 mapper = tvtk.PolyDataMapper()
