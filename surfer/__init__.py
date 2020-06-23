85e9e6ca20586957dbbbf53b188be91380211ff1
Eric Larson <larson.eric.d@gmail.com>
2020-06-23 10:44:12 -0400

    MAINT: Bump version

--- surfer/__init__.py
+++ surfer/__init__.py
@@ -2,7 +2,7 @@ from .viz import Brain, TimeViewer  # noqa
 from .utils import Surface, verbose, set_log_level, set_log_file  # noqa
 from .io import project_volume_data  # noqa
 
-__version__ = "0.11.dev0"
+__version__ = "0.11.0"
 
 set_log_file()  # initialize handlers
 set_log_level()  # initialize logging level
